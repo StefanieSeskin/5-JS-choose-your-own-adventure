6ab7adc4a70cdd838a29eb582e14caee26540cd6
StefanieSeskin <stefanieaseskin@gmail.com>
2021-01-11 19:27:35 -0500

    something is not working

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 
-var theEnd = false
 var stillGoing = true
-var wholeLotta = false
 var endOfSurvey = false
+var theEnd = false
 
 alert ('Welcome to the Led Zeppelin Survey! Click OK to continue.')
     var yesNoNeither = prompt('Do you even like Led Zeppelin?')
 
-if (yesNoNeither === 'no') {
-    theEnd = true
-    alert('Sending Whole Lotta Love anyway.  Bye!')
 
-}
-
-// else if (yesNoEither !'yes' || 'no' )
-//     theEnd = true
-//     alert('You seem Dazed and Confused so Ramble On.  Catch you later!')
-// }
-
-else if (yesNoNeither === 'yes') {
+if (yesNoNeither === 'yes') {
     theEnd = false
     alert('Let\'s Talk about Favorite song, then.  Ok?')
 }
 
+else if (yesNoNeither === 'no') {
+    theEnd = true
+    alert('Sending Whole Lotta Love anyway.  Bye!')
+    
+}
+
+// this no answer doesn't end the survey
+
+else if (yesNoNeither !== 'yes' && yesNoNeither !== 'no') {
+    theEnd = true
+    alert('This was a yes or no question.  Babe I\'m Gonna Leave You to start over!')
+}
+
+// this answer doesn't end the survey
 
 if (!theEnd)
     var faveSong = prompt('Is your favorite song Stairway to Heaven?')
@@ -30,6 +33,8 @@ if (!theEnd)
     alert('Lame.  Time for you to Ramble On.')
     stillGoing = false
 }
+
+// this answer doesn't end the survey
 
 else if (faveSong == 'no') {
     stillGoing = true
@@ -39,13 +44,21 @@ else if (faveSong == 'no') {
 
 
 var wholeLotta = prompt('Is your favorite song Whole Lotta Love?')
-    if (wholeLotta=== 'yes') {
+    if (wholeLotta === 'yes') {
     alert('Mine too!  Rock on!')
+    // theEnd = true
     endOfSurvey = true
     }
 
-else if (wholeLotta = 'no') {
+else if (wholeLotta === 'no') {
     alert("Me either.  I'm a Black Dog gal.  Thanks for playing!")
     endOfSurvey = true
+    // theEnd = true
 
+    } 
+
+else if (wholeLotta !== 'yes' && wholeLotta !== 'no') {
+        endOfSurvey = true
+        // theEnd = true
+        alert('This was a yes or no question.  Guess we had a Communication Breakdown.  Take care!')
     }
