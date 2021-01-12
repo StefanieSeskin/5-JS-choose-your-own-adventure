b5c021c792d75a537d5370a621d65b2e5508edb5
StefanieSeskin <stefanieaseskin@gmail.com>
2021-01-12 15:15:44 -0500

    no

--- main.js
+++ main.js
@@ -6,23 +6,22 @@ var theEnd = false
 alert ('Welcome to the Led Zeppelin Survey! Click OK to continue.')
     var yesNoNeither = prompt('Do you even like Led Zeppelin?')
 
-
-if (yesNoNeither === 'yes') {
-    theEnd = false
-    alert('Let\'s Talk about Favorite song, then.  Ok?')
-}
-
-else if (yesNoNeither === 'no') {
+if (yesNoNeither === 'no') {
     theEnd = true
     alert('Sending Whole Lotta Love anyway.  Bye!')
-    
+    let (yesNoNeither) = 'no'
+
 }
 
-// this no answer doesn't end the survey
+// else if (yesNoNeither !== 'yes' && yesNoNeither !== 'no') {
+//     theEnd = true
+//     alert('This was a yes or no question.  Babe I\'m Gonna Leave You to start over!')
+//     let (yesNoNeither) !== 'yes' && (yesNoNeither) !== 'no'
+// }
 
-else if (yesNoNeither !== 'yes' && yesNoNeither !== 'no') {
-    theEnd = true
-    alert('This was a yes or no question.  Babe I\'m Gonna Leave You to start over!')
+else if (yesNoNeither === 'yes') {
+    theEnd = false
+    alert('Let\'s Talk about Favorite song, then.  Ok?')
 }
 
 // this answer doesn't end the survey
@@ -30,11 +29,13 @@ else if (yesNoNeither !== 'yes' && yesNoNeither !== 'no') {
 if (!theEnd)
     var faveSong = prompt('Is your favorite song Stairway to Heaven?')
     if (faveSong === 'yes') {
-    alert('Lame.  Time for you to Ramble On.')
-    stillGoing = false
+        stillGoing = false
+        alert('Lame.  Time for you to Ramble On.')
+        let (faveSong) = 'yes'
+
 }
 
-// this answer doesn't end the survey
+
 
 else if (faveSong == 'no') {
     stillGoing = true
@@ -46,19 +47,21 @@ else if (faveSong == 'no') {
 var wholeLotta = prompt('Is your favorite song Whole Lotta Love?')
     if (wholeLotta === 'yes') {
     alert('Mine too!  Rock on!')
-    // theEnd = true
-    endOfSurvey = true
+    theEnd = true
+    let (wholeLotta) = 'yes'
+ 
     }
 
+
 else if (wholeLotta === 'no') {
-    alert("Me either.  I'm a Black Dog gal.  Thanks for playing!")
-    endOfSurvey = true
-    // theEnd = true
+    alert('Me either.  I\'m a Black Dog gal.  Thanks for playing!')
+    theEnd = true
+    let (wholeLotta) = 'no'
 
     } 
 
-else if (wholeLotta !== 'yes' && wholeLotta !== 'no') {
-        endOfSurvey = true
-        // theEnd = true
-        alert('This was a yes or no question.  Guess we had a Communication Breakdown.  Take care!')
-    }
+// else if (wholeLotta !== 'yes' && wholeLotta !== 'no') {
+//         alert('This was a yes or no question.  Guess we had a Communication Breakdown.  Take care!')
+//         let (wholeLotta) wholeLotta !== 'yes' && wholeLotta !== 'no'
+//         endOfSurvey = true 
+//     }
